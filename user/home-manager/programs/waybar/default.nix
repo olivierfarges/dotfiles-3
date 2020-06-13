5c3c07549ce74a989ddb82ede559fe12dd903943
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-06-13 03:06:55 -0400

    Implement configuration generation

--- user/home-manager/programs/waybar/default.nix
+++ user/home-manager/programs/waybar/default.nix
@@ -5,7 +5,8 @@ with lib.types;
 let
   cfg = config.programs.waybar;
 
-  isEmpty = list: if list == null then true else length list == 0;
+  # Used when generating warnings
+  modules-path = "programs.waybar.settings.modules";
 
   # Taken from <https://github.com/Alexays/Waybar/blob/adaf84304865e143e4e83984aaea6f6a7c9d4d96/src/factory.cpp>
   default-module-names = [
@@ -18,33 +19,29 @@ let
 
   isValidCustomModuleName = x: elem x default-module-names || (hasPrefix "custom/" x && stringLength x > 7);
   isValidModule = modules: all isValidCustomModuleName (attrNames modules);
-  modules-definition = submodule {
-    options = {
-    };
-  };
-  modules-options = mkOption {
-    description = "Modules configuration";
-    type = addCheck modules-definition isValidModule;
-  };
 
   # This type validates whether modules specified in modules-{left,center,right}
-  # are defaults or have a valid name
+  # are defaults or have a valid name ("custom/whatever")
   modules-names-type = let
-    custom-modules = attrNames cfg.settings.modules;
+    custom-modules = concatMap (x: attrNames x.modules) cfg.settings;
     modules = unique (default-module-names ++ custom-modules);
   in enum modules;
 
   margins = let
     mkMargin = name: {
       "margin-${name}" = mkOption {
-        description = "Margins value without unit";
-        type = int;
+        type = nullOr int;
+        default = null;
         example = 10;
+        description = "Margins value without unit";
       };
     };
-  in mkMerge (map mkMargin [ "top" "left" "bottom" "right" ]);
+    margins = map mkMargin [ "top" "left" "bottom" "right" ];
+  in foldl mergeAttrs {} margins;
 in
 {
+  # meta.maintainers = [ hm.maintainers.berbiche ];
+
   options.programs.waybar = {
     enable = mkEnableOption "Waybar";
 
@@ -84,41 +81,46 @@ in
       type = nullOr (listOf (submodule {
         options = {
           layer = mkOption {
-            type = enum [ "top" "bottom" ];
+            type = nullOr (enum [ "top" "bottom" ]);
+            default = null;
             description = "Decide if the bar is displayed in front (`top`) of the windows or behind (`bottom`)";
             example = "top";
           };
 
           output = mkOption {
             type = nullOr (either str (listOf str));
+            default = null;
+            example = literalExample ''
+              [ "DP-1" "!DP-2" "!DP-3" ]
+            '';
             description = ''
               Specifies on which screen this bar will be displayed.
               Exclamation mark(!) can be used to exclude specific output.
-            '';
-            example = literalExample ''
-              [ "DP-1" "!DP-2" "!DP-3" ]
             '';
           };
 
           position = mkOption {
             type = nullOr (enum [ "top" "bottom" "left" "right" ]);
-            description = "Bar position relative to the output";
+            default = null;
             example = "right";
+            description = "Bar position relative to the output";
           };
 
           height = mkOption {
-            type = ints.unsigned;
+            type = nullOr ints.unsigned;
+            default = null;
             description = "Height to be used by the bar if possible. Leave blank for a dynamic value";
           };
 
           width = mkOption {
             type = nullOr ints.unsigned;
+            default = null;
             description = "Width to be used by the bar if possible. Leave blank for a dynamic value";
           };
 
           modules-left = mkOption {
-            type = modules-names-type;
-            default = [];
+            type = nullOr (listOf modules-names-type);
+            default = null;
             description = "Modules that will be displayed on the left";
             example = literalExample ''
               [ "sway/workspaces" "sway/mode" "wlr/taskbar" ]
@@ -126,8 +128,8 @@ in
           };
 
           modules-center = mkOption {
-            type = modules-names-type;
-            default = [];
+            type = nullOr (listOf modules-names-type);
+            default = null;
             description = "Modules that will be displayed in the center";
             example = literalExample ''
               [ "sway/window" ]
@@ -135,18 +137,46 @@ in
           };
 
           modules-right = mkOption {
-            type = modules-names-type;
-            default = [];
+            type = nullOr (listOf modules-names-type);
+            default = null;
             description = "Modules that will be displayed on the right";
             example = literalExample ''
               [ "mpd" "custom/mymodule#with-css-id" "temperature" ]
             '';
           };
 
-          modules = modules-options;
+          # modules = modules-options;
+          modules = mkOption {
+            type = addCheck attrs isValidModule;
+            default = { };
+            description = "Modules configuration";
+            example = literalExample
+              ''
+                {
+                  "sway/window": {
+                    max-length = 50;
+                  };
+                  "clock": {
+                    format-alt = "{:%a, %d. %b  %H:%M}";
+                  };
+                  "custom/hello-from" = {
+                    format = "hello {}";
+                    max-length = 40;
+                    interval = 10;
+                    # You may be interested in using symlinkJoin to merge all scripts in one derivation
+                    # to have them all under one directory structure in the nix store
+                    exec = pkgs.writeScriptBin "hello-from-waybar" '''
+                      #!${"\${pkgs.bash}/bin/bash"}
+                      echo "from within waybar"
+                    ''';
+                  };
+                }
+              '';
+          };
 
           margin = mkOption {
-            type = str;
+            type = nullOr str;
+            default = null;
             description = "Margins value using the CSS format without units";
             example = "20 5";
           };
@@ -154,13 +184,15 @@ in
           inherit (margins) margin-top margin-left margin-bottom margin-right;
 
           name = mkOption {
-            type = str;
+            type = nullOr str;
+            default = null;
             description = "Optional name added as a CSS class, for styling multiple waybars";
             example = "waybar-1";
           };
 
           gtk-layer-shell = mkOption {
-            type = bool;
+            type = nullOr bool;
+            default = null;
             description = "Option to disable the use of gtk-layer-shell for popups";
           };
         };
@@ -202,36 +234,84 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable (lib.mkMerge [
-    {
-      home.packages = [ pkgs.waybar ];
+  config = let
+      # Inspired by https://github.com/NixOS/nixpkgs/pull/89781
+      writePrettyJSON = name: x:
+        pkgs.runCommandNoCCLocal name { } ''
+        echo '${builtins.toJSON x}' | ${pkgs.jq}/bin/jq . > $out
+      '';
 
-    }
-    (lib.mkIf (cfg.style != null) {
-      xdg.configFile."waybar/style.css".text = cfg.style;
-    })
-    (lib.mkIf cfg.systemd.enable {
-      systemd.user.services.waybar = {
-        Unit = {
-          Description = "Highly customizable Wayland bar for Sway and Wlroots based compositors.";
-          Documentation = "https://github.com/Alexays/Waybar/wiki";
-          PartOf = [ "graphical-session.target" ];
-          Requisite = [ "dbus.service" ];
-          After = [ "dbus.service" ];
-        };
+      configSource = let
+        # Removes nulls because Waybar ignores them for most values
+        removeNulls = filterAttrs (_: v: v != null);
+        # Makes the actual valid configuration Waybar accepts (strips our custom settings before converting to JSON)
+        makeConfiguration = configuration: let
+          # The "modules" option is not valid in the JSON as its descendants have to live at the top-level
+          settings-without-modules = filterAttrs (n: _: n != "modules") configuration;
+          settings-modules =
+            if cfg.settings ? modules
+            then cfg.settings.modules
+            else { };
+        in settings-without-modules // (removeNulls settings-modules);
+        # The clean list of configurations
+        final-configuration = map makeConfiguration cfg.settings;
+      in writePrettyJSON "waybar-config.json" final-configuration;
 
-        Service = {
-          Type = "dbus";
-          BusName = "fr.arouillard.waybar";
-          ExecStart = "${pkgs.waybar}/bin/waybar";
-          Restart = "always";
-          RestartSec = "1sec";
-        };
+      warnings = let
+        mkWarning = idx: name:
+          let i = toString idx; in
+          "The module '${name}' defined in '${modules-path}[definition ${i}-entry ${i}]' is not referenced " +
+          "in neither modules-left, modules-center or modules-right.";
 
-        Install = {
-          WantedBy = [ (if cfg.systemd.withSwayIntegration then "sway-session.target" else "graphical-session.target") ];
-        };
-      };
-    })
-  ]);
+        # Modules declared in `modules` but not referenced in `modules-{left,center,right}`
+        findUnreferencedModules = settings: let
+          all-modules = concatMap (x: let v = settings."modules-${x}"; in if v != null then v else []) ["left" "center" "right"];
+          non-defaults = subtractLists default-module-names all-modules;
+          declared = if settings.modules != null then attrNames settings.modules else [];
+        in subtractLists non-defaults declared;
+        # cfg.settings is a list of Waybar configurations, we need to preserve the index for appropriate warnings
+        all-unreferenced-modules = flip imap1 cfg.settings (i: x: { idx = i; m = findUnreferencedModules x; });
+
+      # Concatenate the list of list of strings (warnings) to a list of strings
+      in flip concatMap all-unreferenced-modules ({ idx, m }: map (mkWarning idx) m);
+
+
+    in # Emitted Nix configuration
+      lib.mkIf cfg.enable (lib.mkMerge [
+        {
+          home.packages = [ pkgs.waybar ];
+        }
+        (lib.mkIf (cfg.settings != null) {
+          # Generate warnings about defined but unreferenced modules
+          inherit warnings;
+
+          xdg.configFile."waybar/config".source = configSource;
+        })
+        (lib.mkIf (cfg.style != null) {
+          xdg.configFile."waybar/style.css".text = cfg.style;
+        })
+        (lib.mkIf cfg.systemd.enable {
+          systemd.user.services.waybar = {
+            Unit = {
+              Description = "Highly customizable Wayland bar for Sway and Wlroots based compositors.";
+              Documentation = "https://github.com/Alexays/Waybar/wiki";
+              PartOf = [ "graphical-session.target" ];
+              Requisite = [ "dbus.service" ];
+              After = [ "dbus.service" ];
+            };
+
+            Service = {
+              Type = "dbus";
+              BusName = "fr.arouillard.waybar";
+              ExecStart = "${pkgs.waybar}/bin/waybar";
+              Restart = "always";
+              RestartSec = "1sec";
+            };
+
+            Install = {
+              WantedBy = [ (if cfg.systemd.withSwayIntegration then "sway-session.target" else "graphical-session.target") ];
+            };
+          };
+        })
+      ]);
 }
