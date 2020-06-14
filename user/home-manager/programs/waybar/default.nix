7efc7e5756df6c6fbfa1aacd076f7581e0e739d6
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-06-13 22:25:34 -0400

    Add warning for undefined modules

--- user/home-manager/programs/waybar/default.nix
+++ user/home-manager/programs/waybar/default.nix
@@ -6,10 +6,10 @@ let
   cfg = config.programs.waybar;
 
   # Used when generating warnings
-  modules-path = "programs.waybar.settings.modules";
+  modulesPath = "programs.waybar.settings.modules";
 
   # Taken from <https://github.com/Alexays/Waybar/blob/adaf84304865e143e4e83984aaea6f6a7c9d4d96/src/factory.cpp>
-  default-module-names = [
+  defaultModuleNames = [
     "sway/mode" "sway/workspaces" "sway/window"
     "wlr/taskbar" "idle_inhibitor"
     "memory" "cpu" "clock" "disk" "tray"
@@ -17,15 +17,8 @@ let
     "pulseaudio" "mpd" "temperature" "bluetooth"
   ];
 
-  isValidCustomModuleName = x: elem x default-module-names || (hasPrefix "custom/" x && stringLength x > 7);
+  isValidCustomModuleName = x: elem x defaultModuleNames || (hasPrefix "custom/" x && stringLength x > 7);
   isValidModule = modules: all isValidCustomModuleName (attrNames modules);
-
-  # This type validates whether modules specified in modules-{left,center,right}
-  # are defaults or have a valid name ("custom/whatever")
-  modules-names-type = let
-    custom-modules = concatMap (x: attrNames x.modules) cfg.settings;
-    modules = unique (default-module-names ++ custom-modules);
-  in enum modules;
 
   margins = let
     mkMargin = name: {
@@ -38,6 +31,126 @@ let
     };
     margins = map mkMargin [ "top" "left" "bottom" "right" ];
   in foldl mergeAttrs {} margins;
+
+  waybarBarConfig = submodule {
+    options = {
+      layer = mkOption {
+        type = nullOr (enum [ "top" "bottom" ]);
+        default = null;
+        description = "Decide if the bar is displayed in front (`top`) of the windows or behind (`bottom`)";
+        example = "top";
+      };
+
+      output = mkOption {
+        type = nullOr (either str (listOf str));
+        default = null;
+        example = literalExample ''
+              [ "DP-1" "!DP-2" "!DP-3" ]
+            '';
+        description = ''
+              Specifies on which screen this bar will be displayed.
+              Exclamation mark(!) can be used to exclude specific output.
+            '';
+      };
+
+      position = mkOption {
+        type = nullOr (enum [ "top" "bottom" "left" "right" ]);
+        default = null;
+        example = "right";
+        description = "Bar position relative to the output";
+      };
+
+      height = mkOption {
+        type = nullOr ints.unsigned;
+        default = null;
+        description = "Height to be used by the bar if possible. Leave blank for a dynamic value";
+      };
+
+      width = mkOption {
+        type = nullOr ints.unsigned;
+        default = null;
+        description = "Width to be used by the bar if possible. Leave blank for a dynamic value";
+      };
+
+      modules-left = mkOption {
+        type = nullOr (listOf str);
+        default = null;
+        description = "Modules that will be displayed on the left";
+        example = literalExample ''
+              [ "sway/workspaces" "sway/mode" "wlr/taskbar" ]
+            '';
+      };
+
+      modules-center = mkOption {
+        type = nullOr (listOf str);
+        default = null;
+        description = "Modules that will be displayed in the center";
+        example = literalExample ''
+              [ "sway/window" ]
+            '';
+      };
+
+      modules-right = mkOption {
+        type = nullOr (listOf str);
+        default = null;
+        description = "Modules that will be displayed on the right";
+        example = literalExample ''
+              [ "mpd" "custom/mymodule#with-css-id" "temperature" ]
+            '';
+      };
+
+      # modules = modules-options;
+      modules = mkOption {
+        type = addCheck attrs isValidModule;
+        default = { };
+        description = "Modules configuration";
+        example = literalExample
+          ''
+            {
+              "sway/window": {
+                max-length = 50;
+              };
+              "clock": {
+                format-alt = "{:%a, %d. %b  %H:%M}";
+              };
+              "custom/hello-from" = {
+                format = "hello {}";
+                max-length = 40;
+                interval = 10;
+                # You may be interested in using symlinkJoin to merge all scripts in one derivation
+                # to have them all under one directory structure in the nix store
+                exec = pkgs.writeScriptBin "hello-from-waybar" '''
+                  #!${"\${pkgs.bash}/bin/bash"}
+                  echo "from within waybar"
+                ''';
+              };
+            }
+          '';
+      };
+
+      margin = mkOption {
+        type = nullOr str;
+        default = null;
+        description = "Margins value using the CSS format without units";
+        example = "20 5";
+      };
+
+      inherit (margins) margin-top margin-left margin-bottom margin-right;
+
+      name = mkOption {
+        type = nullOr str;
+        default = null;
+        description = "Optional name added as a CSS class, for styling multiple waybars";
+        example = "waybar-1";
+      };
+
+      gtk-layer-shell = mkOption {
+        type = nullOr bool;
+        default = null;
+        description = "Option to disable the use of gtk-layer-shell for popups";
+      };
+    };
+  };
 in
 {
   # meta.maintainers = [ hm.maintainers.berbiche ];
@@ -50,7 +163,7 @@ in
         Configuration for Waybar, see <link xlink:href="https://github.com/Alexays/Waybar/wiki/Configuration"/>
         for supported values.
       '';
-      default = null;
+      default = [];
       example = literalExample
         ''
           [
@@ -77,126 +190,8 @@ in
             }
           ]
         '';
-      # not-todo: fix type when https://github.com/NixOS/nixpkgs/pull/75584 is merged
-      type = nullOr (listOf (submodule {
-        options = {
-          layer = mkOption {
-            type = nullOr (enum [ "top" "bottom" ]);
-            default = null;
-            description = "Decide if the bar is displayed in front (`top`) of the windows or behind (`bottom`)";
-            example = "top";
-          };
-
-          output = mkOption {
-            type = nullOr (either str (listOf str));
-            default = null;
-            example = literalExample ''
-              [ "DP-1" "!DP-2" "!DP-3" ]
-            '';
-            description = ''
-              Specifies on which screen this bar will be displayed.
-              Exclamation mark(!) can be used to exclude specific output.
-            '';
-          };
-
-          position = mkOption {
-            type = nullOr (enum [ "top" "bottom" "left" "right" ]);
-            default = null;
-            example = "right";
-            description = "Bar position relative to the output";
-          };
-
-          height = mkOption {
-            type = nullOr ints.unsigned;
-            default = null;
-            description = "Height to be used by the bar if possible. Leave blank for a dynamic value";
-          };
-
-          width = mkOption {
-            type = nullOr ints.unsigned;
-            default = null;
-            description = "Width to be used by the bar if possible. Leave blank for a dynamic value";
-          };
-
-          modules-left = mkOption {
-            type = nullOr (listOf modules-names-type);
-            default = null;
-            description = "Modules that will be displayed on the left";
-            example = literalExample ''
-              [ "sway/workspaces" "sway/mode" "wlr/taskbar" ]
-            '';
-          };
-
-          modules-center = mkOption {
-            type = nullOr (listOf modules-names-type);
-            default = null;
-            description = "Modules that will be displayed in the center";
-            example = literalExample ''
-              [ "sway/window" ]
-            '';
-          };
-
-          modules-right = mkOption {
-            type = nullOr (listOf modules-names-type);
-            default = null;
-            description = "Modules that will be displayed on the right";
-            example = literalExample ''
-              [ "mpd" "custom/mymodule#with-css-id" "temperature" ]
-            '';
-          };
-
-          # modules = modules-options;
-          modules = mkOption {
-            type = addCheck attrs isValidModule;
-            default = { };
-            description = "Modules configuration";
-            example = literalExample
-              ''
-                {
-                  "sway/window": {
-                    max-length = 50;
-                  };
-                  "clock": {
-                    format-alt = "{:%a, %d. %b  %H:%M}";
-                  };
-                  "custom/hello-from" = {
-                    format = "hello {}";
-                    max-length = 40;
-                    interval = 10;
-                    # You may be interested in using symlinkJoin to merge all scripts in one derivation
-                    # to have them all under one directory structure in the nix store
-                    exec = pkgs.writeScriptBin "hello-from-waybar" '''
-                      #!${"\${pkgs.bash}/bin/bash"}
-                      echo "from within waybar"
-                    ''';
-                  };
-                }
-              '';
-          };
-
-          margin = mkOption {
-            type = nullOr str;
-            default = null;
-            description = "Margins value using the CSS format without units";
-            example = "20 5";
-          };
-
-          inherit (margins) margin-top margin-left margin-bottom margin-right;
-
-          name = mkOption {
-            type = nullOr str;
-            default = null;
-            description = "Optional name added as a CSS class, for styling multiple waybars";
-            example = "waybar-1";
-          };
-
-          gtk-layer-shell = mkOption {
-            type = nullOr bool;
-            default = null;
-            description = "Option to disable the use of gtk-layer-shell for popups";
-          };
-        };
-      }));
+      # maybe-todo: improve type when https://github.com/NixOS/nixpkgs/pull/75584 is merged
+      type = listOf waybarBarConfig;
     };
 
     systemd = {
@@ -243,37 +238,56 @@ in
 
       configSource = let
         # Removes nulls because Waybar ignores them for most values
-        removeNulls = filterAttrs (_: v: v != null);
+        removeNulls = filterAttrs (_: v: !isNull v);
         # Makes the actual valid configuration Waybar accepts (strips our custom settings before converting to JSON)
         makeConfiguration = configuration: let
           # The "modules" option is not valid in the JSON as its descendants have to live at the top-level
-          settings-without-modules = filterAttrs (n: _: n != "modules") configuration;
-          settings-modules =
+          settingsWithoutModules = filterAttrs (n: _: n != "modules") configuration;
+          settingsModules =
             if cfg.settings ? modules
             then cfg.settings.modules
             else { };
-        in settings-without-modules // (removeNulls settings-modules);
+        in settingsWithoutModules // (removeNulls settingsModules);
         # The clean list of configurations
-        final-configuration = map makeConfiguration cfg.settings;
-      in writePrettyJSON "waybar-config.json" final-configuration;
+        finalConfiguration = map makeConfiguration cfg.settings;
+      in writePrettyJSON "waybar-config.json" finalConfiguration;
 
       warnings = let
-        mkWarning = idx: name:
-          let i = toString idx; in
-          "The module '${name}' defined in '${modules-path}[definition ${i}-entry ${i}]' is not referenced " +
-          "in neither modules-left, modules-center or modules-right.";
+        mkPath = idx:
+          let i = toString idx;
+          in "${modulesPath}[definition ${i}-entry ${i}]";
+        mkUnreferencedModuleWarning = idx: name:
+          "The module '${name}' defined in '${mkPath idx}' is not referenced " +
+          "in either `modules-left`, `modules-center` or `modules-right` of Waybar's options";
+        mkUndefinedModuleWarning = idx: name:
+          "The module '${name}' defined in '${mkPath idx}' is neither " +
+          "a default module or a custom module declared in '${mkPath idx}.modules'";
 
-        # Modules declared in `modules` but not referenced in `modules-{left,center,right}`
-        findUnreferencedModules = settings: let
-          all-modules = concatMap (x: let v = settings."modules-${x}"; in if v != null then v else []) ["left" "center" "right"];
-          non-defaults = subtractLists default-module-names all-modules;
-          declared = if settings.modules != null then attrNames settings.modules else [];
-        in subtractLists non-defaults declared;
+        # Find all modules in `modules-{left,center,right}` and `modules` not declared/referenced.
         # cfg.settings is a list of Waybar configurations, we need to preserve the index for appropriate warnings
-        all-unreferenced-modules = flip imap1 cfg.settings (i: x: { idx = i; m = findUnreferencedModules x; });
+        allFaultyModules = flip imap1 cfg.settings (
+          idx: settings: let
+            allModules =
+              concatMap (x: let v = settings."modules-${x}"; in if v != null then v else []) ["left" "center" "right"];
+            nonDefaultModules = subtractLists defaultModuleNames allModules;
+            declaredModules = if settings.modules != null then attrNames settings.modules else [];
+            # Modules declared in `modules` but not referenced in `modules-{left,center,right}`
+            unreferencedModules = subtractLists nonDefaultModules declaredModules;
+            # Modules referenced in `modules-{left,center,right}` but not declared in `modules`
+            undefinedModules = subtractLists declaredModules nonDefaultModules;
+          in {
+            idx = idx;
+            undef = undefinedModules;
+            unref = unreferencedModules;
+          });
 
-      # Concatenate the list of list of strings (warnings) to a list of strings
-      in flip concatMap all-unreferenced-modules ({ idx, m }: map (mkWarning idx) m);
+        allWarnings = flip concatMap allFaultyModules (
+          { idx, undef, unref }: let
+            undefined = map (mkUndefinedModuleWarning idx) undef;
+            unreferenced = map (mkUnreferencedModuleWarning idx) unref;
+          in undefined ++ unreferenced
+        );
+      in allWarnings;
 
 
     in # Emitted Nix configuration
@@ -281,7 +295,7 @@ in
         {
           home.packages = [ pkgs.waybar ];
         }
-        (lib.mkIf (cfg.settings != null) {
+        (lib.mkIf (cfg.settings != []) {
           # Generate warnings about defined but unreferenced modules
           inherit warnings;
 
